e8dcd5a65f81015146d6bc1b121c04cb1a125782
Mujo Didic <80248965+MujoDidic@users.noreply.github.com>
2023-04-07 00:13:52 +0200

    Adde new funktion  in js

--- js/site.js
+++ js/site.js
@@ -15,7 +15,7 @@ function getValues() {
     if (Number.isInteger(amountValue) && Number.isInteger(monthsValue) && Number.isInteger(interestValue)) {
         
         //call FizzBuzz
-        let fbArray = fizzBuzzXXL(amountValue, monthsValue, interestValue);
+        let fbArray = loanPayment(amountValue, monthsValue, interestValue);
         
         
         //Display data to a screen
@@ -26,22 +26,36 @@ function getValues() {
        
 }
 
-function fizzBuzzXXL(amountValue, monthsValue, interestValue){
+function loanPayment(amountValue, monthsValue, interestValue){
 
     let returnArray = [];
 
-    //for (let i = 1; i <=100; i++){
-        
-    //    let value = ((i % fizzValue == 0 ? 'Fizz' : '') + (i % buzzValue == 0 ? 'Buzz' : '' ) || i);
-    //    returnArray.push(value);
-    //}
-    //let value = ((amountValue > 0 && monthsValue > 0 && interestValue > 0));
+    let interest = interestValue / 100; // interest u procentima
 
-    if (amountValue > 0 && monthsValue > 0 && interestValue > 0) {
-        //let value = ((amountValue > 0 && monthsValue > 0 && interestValue > 0));
-        returnArray.push(amountValue)
-        returnArray.push(monthsValue)
-        returnArray.push(interestValue)
+    let monthInterest = amountValue * interest / 12; //mjesecna kamata
+
+    
+//--------------------------------------------------------------
+    let totalInterest = 0;
+
+    let restOfLoan = amountValue;
+
+    let pastMonths;
+
+
+    for (pastMonths = 1; pastMonths <= monthsValue; pastMonths++) {
+        
+        let monthRate = restOfLoan / (monthsValue - pastMonths);
+
+        let monthRateWithInterest = monthRate + monthInterest;
+        
+        returnArray.push(pastMonths);
+        returnArray.push(monthRateWithInterest);
+        returnArray.push(monthRate);
+        returnArray.push(monthInterest = restOfLoan * interest / 12);
+        returnArray.push(totalInterest = totalInterest + monthInterest);
+        returnArray.push(restOfLoan = restOfLoan - monthRateWithInterest);
+        
     }
     
     return returnArray;
@@ -60,7 +74,7 @@ function displayData(fbArray){
     //clear the table
     tableBody.innerHTML ="";
 
-    for (let index = 0; index < fbArray.length; index += 3) {
+    for (let index = 0; index < fbArray.length; index += 6) {
         
         let tableBody = document.getElementById("results");
 
@@ -73,16 +87,19 @@ function displayData(fbArray){
         rowCols[0].textContent = fbArray[index];
 
         //rowCols[1].classList.add(fbArray[index + 1]);
-        rowCols[1].textContent = fbArray[index + 1];
+        rowCols[1].textContent = fbArray[index + 1].toFixed(2);
 
        // rowCols[2].classList.add(fbArray[index + 2]);
-        rowCols[2].textContent = fbArray[index + 2];
+        rowCols[2].textContent = fbArray[index + 2].toFixed(2);
 
         //rowCols[3].classList.add(fbArray[index + 3])
-        //rowCols[3].textContent = fbArray[index + 3];
+        rowCols[3].textContent = fbArray[index + 3].toFixed(2);
 
         //rowCols[4].classList.add(fbArray[index + 4]);
-        //rowCols[4].textContent = fbArray[index + 4];
+        rowCols[4].textContent = fbArray[index + 4].toFixed(2);
+
+        //rowCols[4].classList.add(fbArray[index + 4]);
+        rowCols[5].textContent = fbArray[index + 5].toFixed(2);
 
         tableBody.appendChild(tableRow);
     }
